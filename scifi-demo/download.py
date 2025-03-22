3cd5a25ebfcb137c024ba8a7913c8fa47ae07148
roger <luo_hao_jie@hotmail.com>
2025-03-22 17:59:20 +0800

    Add LLM train & inference code

---
+++ scifi-demo/download.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import requests
+import glob
+
+def download_file(url, save_path):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    with open(save_path, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+
+# 下载finetune数据集
+url = "https://huggingface.co/datasets/zxbsmk/webnovel_cn/resolve/main/novel_cn_token512_50k.json?download=true"
+save_path = "data_finetune/scifi-finetune.json"
+if not os.path.exists(save_path):
+    print("Downloading file...")
+    download_file(url, save_path)
+    print("File downloaded successfully.")
+else:
+    print("File already exists.")
+
+# 下载小说合集
+url = "https://huggingface.co/datasets/wzy816/scifi/resolve/main/data.zip?download=true"
+save_path = "data/data.zip"
+if not os.path.exists(save_path):
+    print("Downloading file...")
+    download_file(url, save_path)
+    print("File downloaded successfully.")
+else:
+    print("File already exists.")
+
+
+# 解压zip文件后，将所有txt文件合并成一个文件
+def find_txt_files(folder_path):
+    return glob.glob(os.path.join(folder_path, '**', '*.txt'), recursive=True)
+
+def concatenate_txt_files(txt_files, output_file):
+    with open(output_file, 'w', encoding='utf-8') as output:
+        for txt_file in txt_files:
+            with open(txt_file, 'r', encoding='utf-8') as file:
+                output.write(file.read())
+                output.write('\n')
+
+directiory = 'data'
+output_file = 'data/scifi.all'
+txt_files = find_txt_files(directiory)
+print(f"总文件数量:{len(txt_files)}")
+print(txt_files[:5])
+concatenate_txt_files(txt_files, output_file)
---
+++ scifi-demo/inference.py
@@ -0,0 +1,43 @@
+import os
+import torch
+from model import Model
+
+
+# device = ('mps' if torch.backends.mps.is_available() 
+# else ('cuda' if torch.cuda.is_available() else 'cpu'))
+# print("device:", device)
+TORCH_SEED = 1337
+torch.manual_seed(TORCH_SEED)
+torch.cuda.manual_seed(TORCH_SEED)
+
+# 和训练的流程一样，重复构建同样的词表
+# 加载文本
+with open('data/scifi.all', 'r', encoding='utf-8') as file:
+    text = file.read()
+print(f"文本长度为: {len(text)}")
+print(text[:100])
+
+# 构建词汇表 
+vocab = sorted(list(set(text)))
+vocab_size = len(vocab)
+print(f"词汇表大小为: {vocab_size}")
+char2idx = {char: idx for idx, char in enumerate(vocab)}
+idx2char = {idx: char for char, idx in char2idx.items()}
+encode = lambda x: [char2idx[char] for char in x]
+decode = lambda idxs: ''.join([idx2char[idx] for idx in idxs])
+print(encode("hello world"))
+print(decode(encode("hello world")))
+
+# 构建模型
+model = Model(vocab_size)
+print("loading model...")
+model.load_state_dict(torch.load('model/model-scifi.pt'))
+print("model loaded")
+model.eval()
+
+# 推理
+start = "小明喜欢打篮球，体育很不错，他立志要成为球星。"
+start_ids = encode(start)
+x = torch.tensor(start_ids, dtype=torch.long).unsqueeze(0)
+y = model.generate(x, max_new_tokens=500)
+print(decode(y[0].tolist()))
